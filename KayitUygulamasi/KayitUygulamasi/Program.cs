0d8c292e0a764faa26a9088b4564fec6c989c8e2
OguzDalaslan <104154904+OguzDalaslan@users.noreply.github.com>
2022-04-22 21:28:54 +0300

    KayitUygulamasi

---
+++ KayitUygulamasi/KayitUygulamasi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KayitUygulamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // || veya Demek Biri sağlanırsa if bloguna girer , adres=="" boş mu bırakıldı demek
+            string ad, soyad, adres, ogrencino;
+            Console.WriteLine("Adınızı Giriniz:");
+            ad=Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz:");
+            soyad=Console.ReadLine();
+            Console.WriteLine("Adresinizi Giriniz:");
+            adres=Console.ReadLine();
+            Console.WriteLine("Ögrenci Numaranızı Giriniz");
+            ogrencino=Console.ReadLine();
+            if (ad == "" || soyad == "" || adres == "" || soyad == "")
+                Console.WriteLine("Bilgiler eksik girildi");
+            else
+                Console.WriteLine("Giriş başaralı");
+            Console.ReadKey();
+        }
+    }
+}
